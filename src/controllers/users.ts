63b9bfe84715cab1d6d2aaa4468796016dd0909d
IGOR <igor2311@gmail.com>
2023-04-12 18:18:06 -0300

    FEAT - Adicionado Update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUsers, updateUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -16,6 +16,17 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
         const {id} = req.params;
         const deletedUser = await deleteUserById(id);
         return res.status(200).json(deletedUser).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const {id} = req.params;
+        const {name, email, password} = req.body;
+        const updatedUser = await updateUserById(id, {name, email, password});
+        return res.status(200).json(updatedUser).end();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
